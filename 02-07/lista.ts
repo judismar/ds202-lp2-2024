1e3c7155febb073e6672bce62ee2d43dfa566287
Judismar Arpini Junior <solid.judis@gmail.com>
2024-07-02 19:42:11 -0300

    Create lista.ts

---
+++ 02-07/lista.ts
@@ -0,0 +1,11 @@
+//Assim como em Python, um array e toda sua sintaxe, ambos em Javascript e typescript, funcionam como uma lista em que novos elementos podem ser inseridos.
+//Aqui, definimos o tipo do array (de números, strings ou valores booleanos/lógicos).
+
+var lista: number[];
+number = [2, 3, 5, 7, 11, 13, 17];
+console.log(number);
+
+//Acesso do array/lista: posição 0 a posição n-1.
+//Imprimindo o primeiro número, o número do meio e o último número.
+
+console.log(number[0], number[4], number[6]);
