f2c84f8504dbdd001534dbe2437cce3d72750ee6
Judismar Arpini Junior <solid.judis@gmail.com>
2024-07-05 19:24:15 -0300

    Create exercicio3.ts

---
+++ 05-07/exercicio3.ts
@@ -0,0 +1,7 @@
+//Crie uma tupla para armazenar 3 números, 2 valores booleanos e 2 strings.
+
+var tupla: [number, number, number, boolean, boolean, string, string];
+
+//Exemplo:
+tupla = [3, 4.5, 0, true, false, "olá", ""]
+console.log(tupla);
